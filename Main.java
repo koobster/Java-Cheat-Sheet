5d5b668256b833e3777825ed529e5224fc14763e
koobster <dan.kubrak@gmail.com>
2016-07-17 13:17:16 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,19 +3,14 @@ package com.dankubrak;
 public class Main {
 
     public static void main(String[] args) {
+        // width of 16 (2bytes)
+        char myChar = '\u00A9';
+        char myChar2 = '\u00AE';
+        System.out.println(myChar2);
+        System.out.println(myChar);
 
-        int myMinValue = -2147483647;
-
-        byte Variable1 = 20;
-
-        short Variable2 = 200;
-
-        int Variable3 = 2000;
-
-        long Variable4 = 50000L + 10L * (Variable1 + Variable2 + Variable3);
-        short variable2Total = (short) (1000 + 10 * (Variable1 + Variable2 + Variable3));
-        System.out.println("Variable2Total = " + Variable4);
+        boolean myBoolean = false;
+        boolean isMale = true;
 
     }
 }
-
